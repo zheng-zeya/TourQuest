ff4b205f78aa1930da8c662c06a35f571d8414f3
zhengzeyao <zeyaoyo@gmail.com>
2023-09-18 09:06:31 -0700

    Added /deleteMe api for current users to set their account to inactive in db

--- models/userModel.js
+++ models/userModel.js
@@ -44,6 +44,11 @@ const userSchema = new mongoose.Schema({
     passwordChangedAt: Date,
     passwordResetToken: String,
     passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false,
+    },
 });
 
 userSchema.pre('save', async function (next) {
@@ -62,6 +67,12 @@ userSchema.pre('save', function (next) {
     if (!this.isModified('password') || this.isNew) return next();
 
     this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, function (next) {
+    // this point to current query
+    this.find({ active: { $ne: false } });
     next();
 });
 
